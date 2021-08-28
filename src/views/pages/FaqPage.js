48ffd7dfb5f035514c70a9efc378df3c90cad959
Krishi Sharma <krishi@Krishis-MacBook-Pro.local>
2021-08-28 15:56:59 -0400

    add faq information

--- src/views/pages/FaqPage.js
+++ src/views/pages/FaqPage.js
@@ -1,6 +1,68 @@
+import 'styles/Faq.css';
+
 function FaqPage() {
     return (
-        <h1> This is the faq page </h1>
+        <div className="Faq">
+            <div className="Faq-text">
+                <h1> reply </h1>
+                <h3> How can we RSVP? </h3>
+                <p> When we send invitations, we plan to provide a way to reply by mail or digitally. </p>
+
+                <h3> When is the last date we can RSVP? </h3>
+                <p> The cutoff date is April 8, 2022. The sooner you can let us know, however, the better. </p>
+
+                <h3> What should we do if we can’t make it? </h3>
+                <p> Let us know! It’ll be sad that you won’t be able to make it, but please do let us know. </p>
+
+                <h1> coronavirus </h1>
+                <h3> What coronavirus precautions are in place? </h3>
+                <p> We will have hand sanitizer stations near all events, and will work with vendors to ensure they follow the COVID protocols that are recommended by the CDC at the time of the event. </p>
+
+                <h3> How will changes to the schedule be communicated before the ceremony? </h3>
+                <p> Should the pandemic take a turn for the worse, we plan to communicate any changes to the schedule through this website and email. </p>
+
+                <h3> Are we required to wear masks during the ceremony and reception? </h3>
+                <p> As of right now, we are not planning on requiring masks. This may change as we get closer to the event date, so please keep in mind that this answer may change. </p>
+
+                <h3> Are we required to have a negative test result for coronavirus to attend? </h3>
+                <p> We do not plan to require testing from our guests, however we do recommend getting tested if you are experiencing symptoms the week of the event date. </p>
+
+                <h3> Are we required to have partial or full coronavirus vaccination? </h3>
+                <p> For the safety and comfort of all our guets, we respectfully request guests who are eligible for coronavirus vaccinations to get fully vaccinated. However, we will not require proof of vaccination for attendance.  </p>
+
+                <h1> travel & lodging </h1>
+                <h3> Have you reserved a hotel room block for guests? </h3>
+                <p> Yes, we have blocked rooms for Friday, April 22 and Saturday, April 23 at our venue, the Hyatt Regency Chesapeake Bay Golf Resort, Spa & Marina.  </p>
+
+                <h3> When does the hotel block close? </h3>
+                <p> The last day to book a room in our hotel block is March 9, 2022. Reservations can be made using the hotel website.</p>
+
+                <h3> Are there other hotels nearby to choose from? </h3>
+                <p> There is a Holiday Inn Express, Cambridge that is a 20 minute walk, 5 min drive from our venue. </p>
+
+                <h3> What are the nearby attractions for out of town visitors? </h3>
+                <p> Washington, DC is a two-hour drive from our venue! If you want something a little closer, there is the Choptank River Lighthouse ten minutes away (driving).</p>
+
+                <h1> guests </h1>
+                <h3> Are children allowed? </h3>
+                <p> Absolutely! </p>
+
+                <h3> What should I wear? </h3>
+                <p> As this will be an Hindu/Jain ceremony, we recommend wearing Indian suits to the ceremony and reception! However, we want you to be comfortable, so feel free to wear what makes you happy. </p>
+
+                <h3> Will live streaming options be available for those who can’t make it? </h3>
+                <p> We are looking into how to make this possible - please let us know if you have recommendations. </p>
+
+                <h1> gifts </h1>
+                <h3> Will you be accepting gifts? </h3>
+                <p> Your presence is enough of a present. If you wish to bring a gift, please note that cash is preferred. </p>
+
+                <h3> Should I bring my gift to the wedding? </h3>
+                <p> Yes, we will have a small table at the venue to collect envelopes and any small items. </p>
+
+
+            </div>
+        </div>
     )
 };
 
